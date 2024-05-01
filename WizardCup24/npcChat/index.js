7182bc2e87e4a51c8b106ea446ec73880f824042
buddyswift <146725916+buddyswift@users.noreply.github.com>
2024-05-01 13:24:13 +0100

    Resolved merge conflicts

--- WizardCup24/npcChat/index.js
+++
@@ -1,7 +0,0 @@
-const handleQuery = require('./handleQuery.js');
-const userHasHouseRole = require('./utilities.js').userHasHouseRole;
-
-module.exports = {
-    handleQuery,
-    userHasHouseRole // Exporting for potential external use
-};
